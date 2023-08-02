73e25a85bd905fd1c7235a83624a2f2f224f41a5
phillybebe <philia.hammond@eggseed.com>
2023-08-02 22:43:45 +0000

    created index.js file

---
+++ index.js
@@ -0,0 +1,62 @@
+    const change = document.getElementById("image1");
+
+    change.addEventListener("click", (event) =>{
+      event.target.style.background="purple";
+      event.target.style.color="white";
+    });
+
+    const change2 = document.getElementById("image2");
+
+    change2.addEventListener("click", (event) =>{
+      event.target.style.background="green";
+      event.target.style.color="white";
+    }); 
+
+    const change3 = document.getElementById("image3");
+
+    change3.addEventListener("click", (event) =>{
+      event.target.style.background="blue";
+      event.target.style.color="white";
+    });
+
+    const change4 = document.getElementById("image4");
+
+    change4.addEventListener("click", (event) =>{
+      event.target.style.background="orange";
+      event.target.style.color="white";
+    });
+
+    const change5 = document.getElementById("image5");
+
+    change5.addEventListener("click", (event) =>{
+      event.target.style.background="teal";
+      event.target.style.color="white";
+    });
+
+    const change6 = document.getElementById("image6");
+
+    change6.addEventListener("click", (event) =>{
+      event.target.style.background="brown";
+      event.target.style.color="white";
+    });
+
+    const change7 = document.getElementById("image7");
+
+    change7.addEventListener("click", (event) =>{
+      event.target.style.background="grey";
+      event.target.style.color="white";
+    });
+
+    const change8 = document.getElementById("image8");
+
+    change8.addEventListener("click", (event) =>{
+      event.target.style.background="turquoise";
+      event.target.style.color="white";
+    });
+
+    const change9 = document.getElementById("image9");
+
+    change9.addEventListener("click", (event) =>{
+      event.target.style.background="red";
+      event.target.style.color="white";
+    });
